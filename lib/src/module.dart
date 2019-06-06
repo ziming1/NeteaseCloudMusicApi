77f1a1111c8f112ac58ca6bd06f2d3e2827133df
子明 <51293485+ziming1@users.noreply.github.com>
2019-06-06 17:23:20 +0800

    add api

--- lib/src/module.dart
+++ lib/src/module.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
+import 'dart:math' as math;
 
 import 'package:crypto/crypto.dart';
 import 'package:encrypt/encrypt.dart';
@@ -31,6 +32,17 @@ part 'module/msg.dart';
 part 'module/mv.dart';
 part 'module/personal.dart';
 part 'module/playlist.dart';
+part 'module/playmode.dart';
+part 'module/recommend.dart';
+part 'module/related.dart';
+part 'module/resource_like.dart';
+part 'module/search.dart';
+part 'module/simi.dart';
+part 'module/song.dart';
+part 'module/top.dart';
+part 'module/user.dart';
+part 'module/video.dart';
+part 'module/weblog.dart';
 
 typedef Handler = Future<Answer> Function(Map query, List<Cookie> cookie);
 
@@ -97,4 +109,63 @@ final handles = <String, Handler>{
   "/mv/first": mv_first,
   "/mv/sub": mv_sub,
   "/mv/sublist": mv_sublist,
+  "/personal_fm": personal_fm,
+  "/personalized/djprogram": personalized_djprogram,
+  "/personalized/mv": personalized_mv,
+  "/personalized/newsong": personalized_newsong,
+  "/personalized/privatecontent": personalized_privatecontent,
+  "/personalized": personalized,
+  "/playlist/catlist": playlist_catlist,
+  "/playlist/create": playlist_create,
+  "/playlist/detail": playlist_detail,
+  "/playlist/hot": playlist_hot,
+  "/playlist/subscribe": playlist_subscribe,
+  "/playlist/subscribers": playlist_subscribers,
+  "/playlist/tracks": playlist_tracks,
+  "/playmode/intelligence/list": playmode_intelligence_list,
+  "/program/recommend": program_recommend,
+  "/recommend/resource": recommend_resource,
+  "/recommend/songs": recommend_songs,
+  "/related/allvideo": related_allvideo,
+  "/related/playlist": related_playlist,
+  "/resource/like": resource_like,
+  "/search/hot": search_hot,
+  "/search/multimatch": search_multimatch,
+  "/search/suggest": search_suggest,
+  "/search": search,
+  "/simi/artist": simi_artist,
+  "/simi/mv": simi_mv,
+  "/simi/playlist": simi_playlist,
+  "/simi/song": simi_song,
+  "/simi/user": simi_user,
+  "/song/detail": song_detail,
+  "/song/url": song_url,
+  "/top/album": top_album,
+  "/top/artists": top_artists,
+  "/top/list": top_list,
+  "/top/mv": top_mv,
+  "/top/playlist/highquality": top_playlist_highquality,
+  "/top/playlist": top_playlist,
+  "/top/song": top_song,
+  "/toplist/artist": toplist_artist,
+  "/toplist/detail": toplist_detail,
+  "/toplist": toplist,
+  "/user/audio": user_audio,
+  "/user/cloud_del": user_cloud_del,
+  "/user/cloud_detail": user_cloud_detail,
+  "/user/cloud": user_cloud,
+  "/user/detail": user_detail,
+  "/user/dj": user_dj,
+  "/user/event": user_event,
+  "/user/followeds": user_followeds,
+  "/user/follows": user_follows,
+  "/user/playlist": user_playlist,
+  "/user/record": user_record,
+  "/user/subcount": user_subcount,
+  "/user/update": user_update,
+  "/video/detail": video_detail,
+  "/video/group": video_group,
+  "/video/sub": video_sub,
+  "/video/url": video_url,
+  "/weblog": weblog,
 };
