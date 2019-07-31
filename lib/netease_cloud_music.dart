d26e9a4c8fcc09538ef94b94fa6f52a7545e957a
ziming <51293485+ziming1@users.noreply.github.com>
2019-07-31 13:27:07 +0800

    add decryptor for parameter

--- lib/netease_cloud_music.dart
+++ lib/netease_cloud_music.dart
@@ -3,33 +3,42 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:netease_music_api/src/answer.dart';
-
 import 'src/module.dart';
 
 typedef DebugPrinter = void Function(String message);
+
+typedef QueryParameterDecryptor = Map Function(Map queryParameter);
 
 DebugPrinter debugPrint = (msg) {
   print(msg);
 };
 
-Future<HttpServer> startServer({address = "localhost", int port = 3000}) {
+Future<HttpServer> startServer({
+  address = "localhost",
+  int port = 3000,
+  QueryParameterDecryptor decryptor,
+}) {
   return HttpServer.bind(address, port).then((server) {
     debugPrint("start listen at: http://$address:$port");
     server.listen((request) {
-      debugPrint("request : ${request.uri}");
-      _handleRequest(request);
+      _handleRequest(request, decryptor);
     });
     return server;
   });
 }
 
-void _handleRequest(HttpRequest request) async {
+void _handleRequest(
+    HttpRequest request, QueryParameterDecryptor decryptor) async {
   final handle = handles[request.uri.path];
 
   Answer answer;
   if (handle != null) {
     try {
-      answer = await handle(request.uri.queryParameters, request.cookies);
+      var param = request.uri.queryParameters;
+      if (decryptor != null) {
+        param = decryptor(param);
+      }
+      answer = await handle(param, request.cookies);
     } catch (e, stack) {
       debugPrint(e.toString());
       debugPrint(stack.toString());
@@ -41,4 +50,6 @@ void _handleRequest(HttpRequest request) async {
   request.response.cookies.addAll(answer.cookie);
   request.response.write(json.encode(answer.body));
   request.response.close();
+
+  debugPrint("request[${answer.status}] : ${request.uri}");
 }
